08150e3c943ce719ed6de3bea61487b945fce276
tszlznl <139991256+tszlznl@users.noreply.github.com>
2024-11-12 21:18:29 +0800

    umami链接

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,6 +9,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full bg-background dark:border-slate-50/[0.06] lg:border-b lg:border-slate-900/10">
+      <script defer src="https://umi.417001.xyz/script" data-website-id="13b84e16-16d5-473a-91b6-097a96c6ae61"></script>
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
